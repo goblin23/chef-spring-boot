3436381daa5c0fc086da79a3004e8cdffb0c7006
Etienne Koekemoer <e@ek.pe>
2016-02-16 12:28:21 +0200

    Started adding some tests

--- test/integration/default/serverspec/default_spec.rb
+++ test/integration/default/serverspec/default_spec.rb
@@ -1,9 +1,25 @@
 require 'spec_helper'
 
-describe 'spring-boot-app::default' do
-  # Serverspec examples can be found at
-  # http://serverspec.org/resource_types.html
-  it 'does something' do
-    skip 'Replace this with meaningful tests'
-  end
+describe service('app_0') do
+  it { should_not be_enabled }
+  it { should_not be_running.under('systemd') }
+end
+
+describe service('app_1') do
+  it { should be_enabled }
+  it { should be_running.under('systemd') }
+end
+
+describe port(8091) do
+  it { should be_listening }
+end
+
+describe user('bootapp') do
+  it { should exist }
+  it { should belong_to_group 'bootapp' }
+end
+
+describe user('another_bootapp_user') do
+  it { should exist }
+  it { should belong_to_group 'another_bootapp_group' }
 end
