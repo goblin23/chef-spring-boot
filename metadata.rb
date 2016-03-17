6f908afe1baa88920cadf8c56694c0a159fd5c41
Etienne Koekemoer <e@ek.pe>
2016-03-17 11:08:50 +0200

    Upped version

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,6 @@ maintainer_email 'you@example.com'
 license 'all_rights'
 description 'Installs/Configures spring-boot-app'
 long_description 'Installs/Configures spring-boot-app'
-version '0.2.0'
+version '0.3.0'
 
 depends 'java'
