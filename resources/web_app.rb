f11c696b57692d2d7db40900c4f5d9de75a1cf87
Etienne Koekemoer <e@ek.pe>
2016-03-17 10:34:22 +0200

    Will now work with older versions (12.4.1) of chef-client

--- resources/web_app.rb
+++ resources/web_app.rb
@@ -1,15 +1,15 @@
 actions :uninstall, :install
 default_action :install
 
-property :name, String, name_property: true
-property :user, String, default: 'bootapp'
-property :group, String, default: 'bootapp'
-property :port, Integer, default: 8080
-property :jar_remote_path, String
-property :java_opts, String, default: ""
-property :boot_opts, String, default: ""
-property :logging_directory, String, default: "/var/log"
+attribute :name, kind_of: String
+attribute :user, kind_of: String, default: 'bootapp'
+attribute :group, kind_of: String, default: 'bootapp'
+attribute :port, kind_of: Integer, default: 8080
+attribute :jar_remote_path, kind_of: String
+attribute :java_opts, kind_of: String, default: ''
+attribute :boot_opts, kind_of: String, default: ''
+attribute :logging_directory, kind_of: String, default: '/var/log'
 
-property :wait_for_http, [TrueClass, FalseClass], default: true
-property :wait_for_http_retries, Integer, default: 24
-property :wait_for_http_retry_delay, Integer, default: 5
+attribute :wait_for_http, kind_of: [TrueClass, FalseClass], default: true
+attribute :wait_for_http_retries, kind_of: Integer, default: 24
+attribute :wait_for_http_retry_delay, kind_of: Integer, default: 5
