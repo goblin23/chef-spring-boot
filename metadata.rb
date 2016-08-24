4fbfba9356c4e067b346a0f790434af851e20d17
Etienne Koekemoer <e@ek.pe>
2016-08-24 12:55:23 +0200

    Updated version. Also added github urls for issues

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,9 @@ maintainer_email 'you@example.com'
 license 'Apache License, Version 2.0'
 description 'Installs/Configures spring-boot-app'
 long_description 'Installs/Configures spring-boot-app'
-version '0.3.2'
+version '0.4.0'
 
 depends 'java'
+
+source_url 'https://github.com/EtienneK/chef-spring-boot' if respond_to?(:source_url)
+issues_url 'https://github.com/EtienneK/chef-spring-boot/issues' if respond_to?(:issues_url)
