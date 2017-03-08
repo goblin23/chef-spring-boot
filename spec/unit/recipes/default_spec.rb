985e56534ccea0556d55c8964a5cf1b082568fdd
Guido Schoening <guido.schoening@jambit.com>
2017-03-08 14:39:50 +0100

    converted lwrp into custom resource

--- spec/unit/recipes/default_spec.rb
+++ spec/unit/recipes/default_spec.rb
@@ -6,7 +6,7 @@
 
 require 'spec_helper'
 
-describe 'spring-boot-app::default' do
+describe 'spring-boot::default' do
   context 'When all attributes are default, on an unspecified platform' do
     let(:chef_run) do
       runner = ChefSpec::ServerRunner.new
