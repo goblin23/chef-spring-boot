5e9cca99e412de05cb42f1d2fc97b4a0405d35a8
Guido Schoening <guido.schoening@jambit.com>
2017-03-14 10:43:30 +0100

    moved properties to a variable since literal declaration of hashes does not seem to work

--- recipes/example_init.rb
+++ recipes/example_init.rb
@@ -5,14 +5,14 @@ spring_boot_web_app 'app_0_initd' do
   init_system 'init.d'
   port 9090
 end
-
+props = { 'app_1_initd' => { 'a' => '5', 'b' => '10' }}
 spring_boot_web_app 'app_1_initd' do
   jar_remote_path web_app_jar
   init_system 'init.d'
   user 'another_bootapp_user'
   group 'another_bootapp_group'
   port 9091
-  properties { 'app_1_initd' => { 'a' => '5', 'b' => '10' }}
+  properties props
   java_opts '-Xmx256m -Xms128m'
   boot_opts '--spring.application.name=app_1_initd'
 
