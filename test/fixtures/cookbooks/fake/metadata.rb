7ca2ede8005e0dd2612d2214ba03a89ffa4ee23e
Guido Schöning <guido.schoening@jambit.com>
2020-08-24 16:19:15 +0200

    updated metadata of fixture cookbook for cookstyle

--- test/fixtures/cookbooks/fake/metadata.rb
+++ test/fixtures/cookbooks/fake/metadata.rb
@@ -3,10 +3,9 @@ maintainer              'Guido Schöninig'
 maintainer_email        'guido.schoening@gmail.com'
 license                 'Apache License, Version 2.0'
 description             'Installs/Configures spring-boot-app'
-long_description        'Installs/Configures spring-boot-app'
-source_url              'https://github.com/EtienneK/chef-spring-boot' if respond_to?(:source_url)
-issues_url              'https://github.com/EtienneK/chef-spring-boot/issues' if respond_to?(:issues_url)
-chef_version            '>= 12.1' if respond_to?(:chef_version)
+source_url              'https://github.com/EtienneK/chef-spring-boot'
+issues_url              'https://github.com/EtienneK/chef-spring-boot/issues'
+chef_version            '>= 12.1'
 supports                'centos'
 version                 '0.0.1'
 
